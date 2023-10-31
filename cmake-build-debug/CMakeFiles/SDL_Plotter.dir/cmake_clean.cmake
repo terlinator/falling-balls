f9f8767e309d859aac60d16ebed5c3f522785aa3
AriannaBriel <Ariannabriel314@gmail.com>
2023-10-31 14:01:45 -0500

    Group project thus far. Still need to include the sdl2 and sdl.h libraries

---
+++ cmake-build-debug/CMakeFiles/SDL_Plotter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL_Plotter.dir/SDL_Plotter.cpp.o"
+  "CMakeFiles/SDL_Plotter.dir/SDL_Plotter.cpp.o.d"
+  "CMakeFiles/SDL_Plotter.dir/main.cpp.o"
+  "CMakeFiles/SDL_Plotter.dir/main.cpp.o.d"
+  "SDL_Plotter"
+  "SDL_Plotter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL_Plotter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SDL_Plotter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SDL_Plotter.
+# This may be replaced when dependencies are built.
