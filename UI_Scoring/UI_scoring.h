7fe146f512068392a70a52c452ad9a0860c7493d
AriannaBriel <149217807+AriannaBriel@users.noreply.github.com>
2023-11-20 11:42:57 -0600

    I hate sdl so much. This is the skeleton of the code. I have more on clion, but this is for sure okay.

--- UI_Scoring/UI_scoring.h
+++ UI_Scoring/UI_scoring.h
@@ -1,1 +1,17 @@
+#ifndef GROUPPROJECT_UI_SCORING_H_
+#define GROUPPROJECT_UI_SCORING_H_
+#include <fstream>
+using namespace std;
+
+class scoreTracker{
+private:
+    int score;
+
+public:
+
+    scoreTracker();
+    void incrementScore(int);
+    void displayScore(const ostream&) const;
+};
+#endif GROUPPROJECT_UI_SCORING_H_
 
