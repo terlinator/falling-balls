bc308d4aab192c67c228bd885c6050efb4f85c5c
terlinator <118776472+terlinator@users.noreply.github.com>
2023-12-01 17:33:52 -0600

    Update Blocks.cpp

--- Blocks.cpp
+++ Blocks.cpp
@@ -1,4 +1,21 @@
-  void block::createBlock(){
+struct Color { 
+  int R, G, B;
+  Color(const color color){
+    this->color;
+  }
+  Color(int R=0, int G = 0, int B = 0){
+    this->R = R;
+    this->G = G;
+    this->B = B;
+  }
+  Color operator= (const color color){
+    R = color.R;
+    G = color.G;
+    B = color.B;
+  }
+}
+
+void block::createBlock(){
     color.r = 0;
     color.g = 0;
     color.b = 0;
@@ -8,7 +25,7 @@
   }
 
   void drawBlock(point loc, int size, color c, SDL_Plotter& g){
-
+    
   }
 
   void block::setBlockValue(int val){
