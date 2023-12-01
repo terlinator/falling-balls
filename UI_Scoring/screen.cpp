faf2fa18c1db86cec7d474cebd25320554de7667
AriannaBriel <149217807+AriannaBriel@users.noreply.github.com>
2023-12-01 15:53:25 -0600

    Update screen.cpp

--- UI_Scoring/screen.cpp
+++ UI_Scoring/screen.cpp
@@ -105,3 +105,41 @@ void writeText(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen) {
     }
 }
 
+void writeHeader(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen) {
+    ifstream infile;
+    string line, value;
+    int col, r, g, b, row = 0;
+
+    infile.open("start.csv");
+    while (getline(infile, line)) {
+        stringstream ss(line);
+        col = 0;
+        while (getline(ss, value, ',')) {
+            int pixelValue = stoi(value);
+            int pixelSize = SizeOfFont;
+
+            // Prints black if number is 1, white for anything else
+            if(pixelValue == 1){
+                r = 0;
+                g = 0;
+                b = 0;
+            }
+            else{
+                r = 255;
+                g = 255;
+                b = 255;
+            }
+
+            // Draw the pixel
+            for (int x = 0; x < pixelSize; ++x) {
+                for (int y = 0; y < pixelSize; ++y) {
+                    homeScreen.plotPixel(posX + col * pixelSize + x, posY + row * pixelSize + y, r, g, b);
+                }
+            }
+
+            col++;
+        }
+
+        row++;
+    }
+}
