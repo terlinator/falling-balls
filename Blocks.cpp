7456097eb6cb25bfc959fbe5279db206b6e360a7
terlinator <118776472+terlinator@users.noreply.github.com>
2023-12-01 17:19:42 -0600

    Update Blocks.cpp

--- Blocks.cpp
+++ Blocks.cpp
@@ -1,12 +1,29 @@
   void block::createBlock(){
+    color.r = 0;
+    color.g = 0;
+    color.b = 0;
+    loc.x = 0;
+    loc.y = 0;
+    size = 10;
+  }
+
+  void drawBlock(point loc, int size, color c, SDL_Plotter& g){
 
   }
-  void drawBlock(point loc, int size, color c, SDL_Plotter& g);
 
-  void setBlockValue(int val);
-  int getBlockValue();
+  void block::setBlockValue(int val){
+    
+  }
 
-  vector<point> getEdge();
+  int block::getBlockValue(){
+
+  }
+
+  vector<point> block::getEdge(){
+
+  }
   
-  bool collisionCheck(point);
+  bool block::collisionCheck(point){
+    if 
+  }
   
