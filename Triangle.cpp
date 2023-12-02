9cae91496d1a871b51607852f9e9f48f2ebc0744
CamKHardin <150968844+CamKHardin@users.noreply.github.com>
2023-12-01 18:27:33 -0600

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -18,10 +18,24 @@ void Triangle::setPoints(vector<Point> vec) {
   this->points = vec;
 }
 
-void Triangle::drawTriangle(point loc, int size, color c, SDL_Plotter& g) {
+void Triangle::drawTriangle(point origin, int size, color c, SDL_Plotter& g) {
+  //plotPixel(p.x,  p.y,  r,  g,  b)
+  //for square
+  int squareX, squareY;
+  int R = this->color.R;
+  int G = this->color.G;
+  int B = this->color.B;
   
+  for(int i = -sidelength/2; i < idelength/2; i++){   //how far away (rows)
+    for(int j = -sidelength/2; j < idelength/2; j++){ //how far away (columns)
+      squareX = (origin.x)-i;  //x plus or minus distance
+      squareY = (origin.y)-j;  //y plus or minus distance
+      g.plotPixel(squareX,squareY,R,G,B); 
+    }
+  }
 }
 
 vector<Point> Triangle::getEdge() {
+  
 }
 
