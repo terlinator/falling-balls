d2cf2d0d92f86ab3eb9a6a2fcba3079e0114c291
terlinator <118776472+terlinator@users.noreply.github.com>
2023-12-02 17:26:09 -0600

    Update Blocks.cpp

--- Blocks.cpp
+++ Blocks.cpp
@@ -36,19 +36,27 @@ void block::createBlock(){
 
   square block::getEdge(){
     vector<vector <point> > edges;
+    int minX = square[0][0];
+    int maxX = square[0][0];
     for(int i = 0; i < square.size(); i++){ //Loops through the vector for the points 
-        for(int j = 0; k < square.size(); j++){  
-          int min = square(0).getY; //sets the first variable to the min
-          if(square(i).getY < min){ 
-              min = square(i).getY; //Makes the smaller variable the min point in the vector
-          }
-          int max = square(0).getY;
-          if(square(i).getY > max){
-               max = square(i).getY; //Makes the greater variable the max point in the vector
-          }
-          edges.push_back(point(minX,minY));
-          edges.push_back(point(maxX,maxY));
+        if(square[i][j] < minX){
+            minX = square[i][j];
         }
+        if(square[i][j] > maxX){
+            maxX = square[i][j];
+        }
+        for(int j = 0; k < square.size(); j++){  
+          int minY = square[i][0]; //sets the first variable to the min
+          if(square[i][j] < min){ 
+              min = square[i][j]; //Makes the smaller variable the min point in the vector
+          }
+          int maxY = square[i][0];
+          if(square[i][j] > max){
+               max = square[i][j]; //Makes the greater variable the max point in the vector
+          }
+        }
+        edges.push_back(point(minX,minY));
+        edges.push_back(point(maxX,maxY));
     return edges;
   }
 
