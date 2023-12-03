d363c1b8f201a86328d7f5bf2d294716080e00ba
Sterling Matthews <sterlingmatthews8@gmail.com>
2023-12-03 15:53:02 -0600

    made some changes to how to find collision

--- Blocks.cpp
+++ Blocks.cpp
@@ -1,85 +1,108 @@
-#include "point.h"
 #include "Color.h"
+#include "Blocks.h"
+#include "ball/ball.h"
 
-void block::createBlock(){
-    R = 0;
-    G = 0;
-    B = 0;
-    loc.x = 0;
-    loc.y = 0;
-    size = 10;
-  }
+block::block() {
+    Color c;
+    x = 0;
+    y = 0;
+    sideLength = 10;
+    health = 1;
+}
 
-  void Square:drawSquare(point origin, int sidelength, color c, SDL_Plotter& g) {
+void block::drawBlock(point origin, int sidelength, color c, SDL_Plotter& g) {
     //plotPixel(p.x,  p.y,  r,  g,  b)
     //for square
     int squareX, squareY;
-    int R = this->color.R;
-    int G = this->color.G;
-    int B = this->color.B;
-  
-    for(int i = -sidelength/2; i < sidelength/2; i++){   //how far away (rows)
-      for(int j = -sidelength/2; j < sidelength/2; j++){ //how far away (columns)
-        squareX = (origin.x)-i;  //x plus or minus distance
-        squareY = (origin.y)-j;  //y plus or minus distance
-        g.plotPixel(squareX,squareY,R,G,B); 
-      }
+    int R = c.R;
+    int G = c.G;
+    int B = c.B;
+
+    for (int i = -sidelength / 2; i < sidelength / 2; i++) {
+        //how far away (rows)
+        for (int j = -sidelength / 2; j < sidelength / 2; j++) {
+            //how far away (columns)
+            squareX = (origin.x) - i; //x plus or minus distance
+            squareY = (origin.y) - j; //y plus or minus distance
+            g.plotPixel(squareX, squareY, R, G, B);
+        }
     }
-  }
+}
 
-  void block::setBlockValue(int val){
+void block::setBlockValue(int val) {
     health = val;
-  }
+}
+void block::setSide(int x) {
+    sideLength = x;
+}
 
-  int block::getBlockValue(){
+int block::getSideLength() {
+    return sideLength;
+}
+
+int block::getBlockValue() {
     return health;
-  }
+}
 
-  square block::getEdge(vector<vector<point> > edges){
+bool block::collisionCheck(Ball circle) {
+    bool Tf;
+    if(circle.getRadius() > sideLength) {
+        Tf = false;
+    }
+    else {
+        Tf = true;
+    }
+    return Tf;
+}
+
+
+/*
+ *vector<point> block::getEdge(vector<vector<point>> edges) {
     int minX = edges[0][0];
     int maxX = edges[0][0];
-    for(int i = 0; i < square.size(); i++){ //Loops through the vector for the points 
-        if(edges[i][0] < minX){
+    for (int i = 0; i < square.size(); i++) {
+        //Loops through the vector for the points
+        if (edges[i][0] < minX) {
             minX = edges[i][0];
         }
-        if(edges[i][0] > maxX){
+        if (edges[i][0] > maxX) {
             maxX = edges[i][0];
         }
-        for(int j = 0; j < square.size(); j++){  
-          int minY = edges[i][0]; //sets the first variable to the min
-          if(edges[i][j] < minY){ 
-              minY = edges[i][j]; //Makes the smaller variable the min point in the vector
-          }
-          int maxY = edges[i][0];
-          if(edges[i][j] > maxY){
-               maxY = edges[i][j]; //Makes the greater variable the max point in the vector
-          }
+        for (int j = 0; j < square.size(); j++) {
+            int minY = edges[i][0]; //sets the first variable to the min
+            if (edges[i][j] < minY) {
+                minY = edges[i][j]; //Makes the smaller variable the min point in the vector
+            }
+            int maxY = edges[i][0];
+            if (edges[i][j] > maxY) {
+                maxY = edges[i][j]; //Makes the greater variable the max point in the vector
+            }
         }
-        edges.push_back(point(minX,minY));
-        edges.push_back(point(maxX,maxY));
+        edges.push_back(point(minX, minY));
+        edges.push_back(point(maxX, maxY));
+    }
     return edges;
-  }
+}
+*/
 
-  vector<vector<Point> > groupPointsByX(const vector<Point> points) {
+vector<vector<point>> groupPointsByX(const vector<point> points) {
     //Find the maximum column to determine the range of cols
     int maxCol = 0;
     for (int i = 0; i < points.size(); i++) {
-        const Point& point = points[i];
+        const point& point = points[i];
         maxCol = max(maxCol, point.x);
     }
 
     //Create cols to hold points with the same col value
-    vector<vector<Point> > cols(maxCol + 1);
+    vector<vector<point>> cols(maxCol + 1);
 
     //Group points into cols based on their x values
     for (int i = 0; i < points.size(); i++) {
-        const Point& point = points[i];
+        const point& point = points[i];
         cols[point.x].push_back(point);
     }
     return cols;
-  }
-  
-  bool block::collisionCheck(point){
-    if 
-  }
+}
+
+
   
--- Blocks.h
+++ Blocks.h
@@ -1,23 +1,30 @@
 #ifndef BLOCKS_H_
 #define BLOCKS_H_
+#include "SDL_Plotter/SDL_Plotter.h"
+#include "ball/ball.h"
 
-class block(){
+class block{
   private:
   int x, y;
-  vector<point> square;
+  std::vector<point> square;
   int health;
-  
+  int sideLength;
+
   public:
+  block();
   void createBlock();
+  void setSide(int x);
   void drawBlock(point loc, int size, color c, SDL_Plotter& g);
 
+  int getSideLength();
   void setBlockValue(int val);
   int getBlockValue();
 
-  vector<point> getEdge();
+  bool collisionCheck(Ball circle);
+  vector<point> getEdge(vector<vector<point>> edges);
   
-  bool collisionCheck(point);
+
   
 };
 
-#endif //BLOCK_H_
+#endif //BLOCKS_H_
