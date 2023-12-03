afc1e070b3cecfaad0206b60607bac6984362e63
AriannaBriel <149217807+AriannaBriel@users.noreply.github.com>
2023-12-03 13:37:09 -0600

    Update UI_scoring.h

--- UI_Scoring/UI_scoring.h
+++ UI_Scoring/UI_scoring.h
@@ -1,11 +1,26 @@
-#ifndef UI_SCORING_H_
-#define UI_SCORING_H_
+#ifndef GROUPPROJECT_UI_SCORING_H
+#define GROUPPROJECT_UI_SCORING_H
+// 12/3/23 Briel 1:35 PM
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
 
-class scoreTotal(){
-private:
+// testing to make sure the count is right
+struct player {
+    player();
 
+    player(const string &, int);
 
-public:
-}
+    void setScore(int);
+    int getScore() const;
+    static bool compareScores(const player&, const player&);
+    void updateLeaderboard(const string&, int);
+    void playGame();
+    string name;
+    int score;
+};
 
-#endif UI_SCORING_H_
+#endif //GROUPPROJECT_UI_SCORING_H
