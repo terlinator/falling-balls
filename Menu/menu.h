83184ba870c5e9cec6d450bd705549cae17fbfd2
DanielCompSciBaylor <150971403+DanielCompSciBaylor@users.noreply.github.com>
2023-12-03 15:33:48 -0600

    Update menu.h

--- Menu/menu.h
+++ Menu/menu.h
@@ -1,1 +1,8 @@
 #include <iostream>
+
+#include "screen.h"
+
+void writeText(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen);
+
+void writeHeader(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen);
+
