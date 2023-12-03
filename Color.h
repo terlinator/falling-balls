57928a21f7f3d1e6d3ab8ed251c53c0703d7234d
terlinator <118776472+terlinator@users.noreply.github.com>
2023-12-03 16:35:10 -0600

    Update Color.h
    new updates to color

--- Color.h
+++ Color.h
@@ -1,13 +1,15 @@
 /* Cam - 12/1/23 5:33
 */
+#ifndef COLOR_H_
+#define COLOR_H_
 
-Struct Color {
+struct Color {
   int R, G, B;
 
   Color(const Color& color) {
     *this = color;
   }
-  Color(int R = 0; int G = 0; int B = 0) {
+  Color(int R = 0, int G = 0, int B = 0) {
     this->R = R;
     this->G = G;
     this->B = B;
@@ -18,4 +20,6 @@ Struct Color {
     B = color.B;
     return *this;
   }
-}
+};
+
+#endif COLOR_H_
