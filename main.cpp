b9cf89a7527e67103f222b28653c1982c7dd222f
AriannaBriel <149217807+AriannaBriel@users.noreply.github.com>
2023-12-03 13:45:18 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,12 +9,25 @@ void drawCircle(point loc, int size, color c, SDL_Plotter& g);
 int main(int argc, char ** argv)
 {
 
-    SDL_Plotter g(1000,1000);
+  SDL_Plotter g(SCREEN_SIZE_WIDTH,SCREEN_SIZE_HEIGHT);
+    char key;
+    Ball ball;
     point p;
     color c;
     int size;
     Uint32 RGB;
+    
+ // (position x, position y, font size, screen)
+    writeFallingBalls(130, 100, 12, g);
+    writeStart(280, 390, 4, g);
 
+
+    for (int round = 0; round < 10; round++) {
+        // (x coordinate, y coordinate), size, (r, g, b), g)
+        drawCircle({850, 800}, 50, {255, 0, 0}, g); // red
+        drawRectangle({500, 800}, 50, {255, 0, 0}, g);
+        drawCircle({150, 800}, 50, {255, 0, 0}, g);
+    }
     while (!g.getQuit())
     {
 
@@ -25,20 +38,22 @@ int main(int argc, char ** argv)
             }
         }
 
-        if(g.mouseClick()){
+        if (g.mouseClick()) {
             p = g.getMouseClick();
-            size = rand()%50;
-            c.R  = rand()%256;
-            c.G  = rand()%256;
-            c.B  = rand()%256;
-            drawCircle(p, size, c, g);
+            ball.setLocation(p);
+            g.clear();
         }
         g.update();
-        
+
+        ball.display(g, true);
+        ball.move();
+        ball.display(g, false);
+
+        g.Sleep(4);
     }
+
+    return 0;
 }
-
-
 void drawCircle(point loc, int size, color c, SDL_Plotter& g){
     for(double i = -size; i <= size;i+=0.1){
         for(double j = -size; j <= size; j+=0.1){
