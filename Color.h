161bd5d528f3c346ea0c4c9bfa54e0a61c737fec
CamKHardin <150968844+CamKHardin@users.noreply.github.com>
2023-12-04 09:34:07 -0600

    Update Color.h

--- Color.h
+++ Color.h
@@ -1,5 +1,5 @@
-/* Cam - 12/1/23 5:33
-*/
+/* Cam - 12/1/23 5:33 */
+
 #ifndef COLOR_H_
 #define COLOR_H_
 
@@ -9,11 +9,15 @@ struct Color {
   Color(const Color& color) {
     *this = color;
   }
+
+  //Color name(R,G,B) constructor;
   Color(int R = 0, int G = 0, int B = 0) {
     this->R = R;
     this->G = G;
     this->B = B;
   }
+
+  //Overload = operator to be able to say color1 = color2
   Color& operator= (const Color& color) {
     R = color.R;
     G = color.G;
