dfda558af230927e1d69e4e6c3c74e4eb0c3e575
AOM3232 <150971628+AOM3232@users.noreply.github.com>
2023-12-05 16:24:14 -0600

    Update Force.h
    Added documentation.

--- Force/Force.h
+++ Force/Force.h
@@ -1,3 +1,10 @@
+//Group Twelve Names: Daniel Esquivel, Briel Finley, 
+//	Cameron Hardin, Sterling Matthews, Andrew Meador
+//Project Name: Falling Balls
+//File Name: Force.h
+//File Created: 11/14/2023
+//File Last Edited: 12/05/2023, Andrew Meador
+
 #ifndef FORCE_H_
 #define FORCE_H_
 #include <cmath>
@@ -10,22 +17,66 @@ const double PI = 3.14159265358979323846264338327950;
 class force{
 private:
     double magnitude;
-    double direction;  // radian
+    double direction;  // radians
 
 public:
+    //description: default constructor
+    //return: force object
+    //precondition: null
+    //postcondition: a force object is created
     force();
+    //description: constructor with parameters
+    //return: force object
+    //precondition: two doubles exist
+    //postcondition: a force object with non-default values is created
     force(double m, double d);
 
+    //description: sets magnitude member
+    //return: void
+    //precondition: a force object exists
+    //postcondition: the magnitude of a force object is changed
     void setMagnitude(double m);
+    //description: sets direction member
+    //return: void
+    //precondition: a force object exists
+    //postcondition: the direction of a force object is changed
     void setDirection(double d);
+    //description: returns magnitude
+    //return: double
+    //precondition: a force object exists
+    //postcondition: magnitude value remains unchanged
     double getMagnitude()const;
+    //description: returns direction
+    //return: double
+    //precondition: a force object exists
+    //postcondition: direction value remains unchanged
     double getDirection()const;
 
+    //description: allows for adding force objects
+    //return: force object
+    //precondition: two force objects exist
+    //postcondition: members of two force objects are added together
     force operator+(const force& other) const;
+    //description: allows for setting force objects to one another
+    //return: force object
+    //precondition: two force objects exist
+    //postcondition: one force object is set to another
     force operator=(const force& other);
+    //description: calculates and applies force to an object
+    //return: force object
+    //precondition: two force objects exist
+    //postcondition: members of two force objects are calculated together
     force add(const force& other) const;
+    //description: gives force to an object
+    //return: void
+    //precondition: a force object exists
+    //postcondition: force object values remain unchanged
     void apply(const force& other);
 
+    //description: displays force values being used
+    //return: void
+    //precondition: a force object and ostream exist
+    //postcondition: force object values remain unchanged
     void display(ostream& os)const{
         os << getMagnitude() << " " << getDirection() << endl;
     }
