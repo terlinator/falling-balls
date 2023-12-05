c5fab6c287aaf177eb404edb128a1252eb671ee0
CamKHardin <150968844+CamKHardin@users.noreply.github.com>
2023-12-05 16:18:07 -0600

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -1,1 +1,30 @@
+#ifndef CIRCLE_H_
+#define CIRCLE_H_
 
+#include "SDL_Plotter/SDL_Plotter.h"
+
+class Circle{
+  private:
+  Point loc;
+  int health;
+  int radius;
+  Color color;
+
+  public:
+  Circle();
+  Circle(Point, int, int, Color); //Loc, radius, health, color
+  void drawCircle(const Triangle&, SDL_Plotter&);
+
+  Point getLocation();
+  void setLocation(point x);
+
+  int getRadius();
+  void setRadius(int x);
+
+  int getHealth();
+  void setHealth(int val);
+
+  bool collisionCheck(const Ball&);
+};
+
+#endif //Circle_h
