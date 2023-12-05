71c57bf74b029050db623919a6e49136896340bc
CamKHardin <150968844+CamKHardin@users.noreply.github.com>
2023-12-05 17:45:20 -0600

    Update Blocks.cpp

--- Blocks.cpp
+++ Blocks.cpp
@@ -1,6 +1,7 @@
-#include "blocks.h"
-#include "ball.h"
-#include "SDL_Plotter.h"
+
+
+#include "Blocks.h"
+//#include the sdl plotter
 
 Block::Block() {
   this->loc.x = 80;
@@ -8,12 +9,48 @@ Block::Block() {
 
   this->health = 1;
   this->sideLength = 10;
-  this->colorType = color(0,0,0);
+  this->color1 = color(0,0,0);
 }
 
-void Block::drawBlock(point origin, int sidelength, color c, SDL_Plotter& g) {
+Block::Block(Point p, int size, int health, color c) {
+  this->loc.x = p.x;
+  this->loc.y = p.y;
+
+  this->sideLength = size;
+  this->health = health;
+  this->color1 = color(c.R,c.G,c.B);
+}
+
+//Location Functions
+Point Block::getLoc() const {
+  return this->loc;
+}
+
+void Block::setLoc(Point p) {
+  this->loc = p;
+}
+
+//Side Functions
+int Block::getSide() const{
+  return this->sideLength;
+}
+
+void Block::setSide(int x) {
+  this->sideLength = x;
+}
+
+//Health Functions
+int Block::getHealth() const {
+  return this->health;
+}
+
+void Block::setHealth(int value) {
+  this->health = value;
+}
+
+void Block::drawBlock(SDL_Plotter& g) {
     //plotPixel(p.x,  p.y,  r,  g,  b)
-    //for square
+    Point p = getLoc();
     int squareX, squareY;
     int R = c.R;
     int G = c.G;
@@ -23,33 +60,16 @@ void Block::drawBlock(point origin, int sidelength, color c, SDL_Plotter& g) {
         //how far away (rows)
         for (int j = -sidelength / 2; j < sidelength / 2; j++) {
             //how far away (columns)
-            squareX = (origin.x) - i; //x plus or minus distance
-            squareY = (origin.y) - j; //y plus or minus distance
+            squareX = (p.x) - i; //x plus or minus distance
+            squareY = (p.y) - j; //y plus or minus distance
             g.plotPixel(squareX, squareY, R, G, B);
         }
     }
 }
 
-void Block::setBlockValue(int val) {
-    health = val;
-}
-void Block::setSide(int x) {
-    sideLength = x;
-}
 
-point Block::getLocation() {
-    return loc;
-}
 
-int Block::getSideLength() {
-    return sideLength;
-}
-
-int Block::getBlockValue() {
-    return health;
-}
-
-bool Block::collisionCheck(Ball circle) {
+/*bool Block::collisionCheck(Ball circle) {
     bool Tf;
     //dist = sqrt((x2 - x1)^2 + (y2 - y1)^2 )
     if(circle.getRadius() + circle.getLocation().x > ((sideLength / 2) + this->getLocation().x)) {
@@ -70,55 +90,4 @@ bool Block::collisionCheck(Ball circle) {
         Tf = false;
     }
     return Tf;
-}
-
-
-/*
- *vector<point> block::getEdge(vector<vector<point>> edges) {
-    int minX = edges[0][0];
-    int maxX = edges[0][0];
-    for (int i = 0; i < square.size(); i++) {
-        //Loops through the vector for the points
-        if (edges[i][0] < minX) {
-            minX = edges[i][0];
-        }
-        if (edges[i][0] > maxX) {
-            maxX = edges[i][0];
-        }
-        for (int j = 0; j < square.size(); j++) {
-            int minY = edges[i][0]; //sets the first variable to the min
-            if (edges[i][j] < minY) {
-                minY = edges[i][j]; //Makes the smaller variable the min point in the vector
-            }
-            int maxY = edges[i][0];
-            if (edges[i][j] > maxY) {
-                maxY = edges[i][j]; //Makes the greater variable the max point in the vector
-            }
-        }
-        edges.push_back(point(minX, minY));
-        edges.push_back(point(maxX, maxY));
-    }
-    return edges;
-}
-*/
-
-/*vector<vector<point>> groupPointsByX(const vector<point> points) {
-    //Find the maximum column to determine the range of cols
-    int maxCol = 0;
-    for (int i = 0; i < points.size(); i++) {
-        const point& point = points[i];
-        maxCol = max(maxCol, point.x);
-    }
-
-    //Create cols to hold points with the same col value
-    vector<vector<point>> cols(maxCol + 1);
-
-    //Group points into cols based on their x values
-    for (int i = 0; i < points.size(); i++) {
-        const point& point = points[i];
-        cols[point.x].push_back(point);
-    }
-    return cols;
-}
-*/
-
+}*/
