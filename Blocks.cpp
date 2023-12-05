cd3e3501cabeda65ff19ee5aa1b39b9b80e31ce9
AOM3232 <150971628+AOM3232@users.noreply.github.com>
2023-12-05 17:28:51 -0600

    Update Blocks.cpp
    Working in Andrew's IDE

--- Blocks.cpp
+++ Blocks.cpp
@@ -1,12 +1,14 @@
-#include "Blocks.h"
-//#include the sdl plotter
-block::Block() {
+#include "blocks.h"
+#include "ball.h"
+#include "SDL_Plotter.h"
+
+Block::Block() {
   this->loc.x = 80;
   this->loc.y = 80;
 
   this->health = 1;
   this->sideLength = 10;
-  this->color = color(0,0,0);
+  this->colorType = color(0,0,0);
 }
 
 void Block::drawBlock(point origin, int sidelength, color c, SDL_Plotter& g) {
@@ -35,12 +37,12 @@ void Block::setSide(int x) {
     sideLength = x;
 }
 
-int Block::getSideLength() {
-    return sideLength;
-}
-
 point Block::getLocation() {
     return loc;
+}
+
+int Block::getSideLength() {
+    return sideLength;
 }
 
 int Block::getBlockValue() {
@@ -70,4 +72,53 @@ bool Block::collisionCheck(Ball circle) {
     return Tf;
 }
 
-  
+
+/*
+ *vector<point> block::getEdge(vector<vector<point>> edges) {
+    int minX = edges[0][0];
+    int maxX = edges[0][0];
+    for (int i = 0; i < square.size(); i++) {
+        //Loops through the vector for the points
+        if (edges[i][0] < minX) {
+            minX = edges[i][0];
+        }
+        if (edges[i][0] > maxX) {
+            maxX = edges[i][0];
+        }
+        for (int j = 0; j < square.size(); j++) {
+            int minY = edges[i][0]; //sets the first variable to the min
+            if (edges[i][j] < minY) {
+                minY = edges[i][j]; //Makes the smaller variable the min point in the vector
+            }
+            int maxY = edges[i][0];
+            if (edges[i][j] > maxY) {
+                maxY = edges[i][j]; //Makes the greater variable the max point in the vector
+            }
+        }
+        edges.push_back(point(minX, minY));
+        edges.push_back(point(maxX, maxY));
+    }
+    return edges;
+}
+*/
+
+/*vector<vector<point>> groupPointsByX(const vector<point> points) {
+    //Find the maximum column to determine the range of cols
+    int maxCol = 0;
+    for (int i = 0; i < points.size(); i++) {
+        const point& point = points[i];
+        maxCol = max(maxCol, point.x);
+    }
+
+    //Create cols to hold points with the same col value
+    vector<vector<point>> cols(maxCol + 1);
+
+    //Group points into cols based on their x values
+    for (int i = 0; i < points.size(); i++) {
+        const point& point = points[i];
+        cols[point.x].push_back(point);
+    }
+    return cols;
+}
+*/
+
