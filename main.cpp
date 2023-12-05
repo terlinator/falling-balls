67ea631ed05332badf5ac62d47c1f8f07cfc3123
AOM3232 <150971628+AOM3232@users.noreply.github.com>
2023-12-05 17:22:53 -0600

    Update main.cpp
    Working on Andrew's IDE

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
+//Group Twelve Names: Daniel Esquivel, Briel Finley,
+//	Cameron Hardin, Sterling Matthews, Andrew Meador
+//Project Name: Falling Balls
+//File Name: main.cpp
+//File Created: 11/14/2023
+//File Last Edited: 12/05/2023, Andrew Meador
+
 #include <iostream>
 //#include "UI_Scoring/UI_Scoring.h"
 #include "constants.h"
-#include "UI_Scoring/screen.h"
+#include "screen.h"
 #include <cstdlib>
-#include "ball/ball.h"
+#include "ball.h"
+#include "blocks.h"
+#include "Triangle.h"
+#include "SDL_Plotter.h"
 
 using namespace std;
 
@@ -15,25 +25,29 @@ int main(int argc, char ** argv){
     color c;
     int size;
     Uint32 RGB;
+    Block block;
+    Triangle triangle;
+    color color1(0, 255, 0);
+
 
     // g.getColor(10,10);
     point l (850,800);
     point j (500,800);
     point k (150,800);
 
-    color titleColor = {0,  255, 0};     // Green
-    color backgroundColor = {0, 0, 0};    // Black
-    color playButtonColor = {255, 0, 0};  // Red
-    color playButtonTextColor = {255, 255, 255}; // White
+    color titleColor(0, 255, 0);     // Green
+    color backgroundColor(0, 0, 0);    // Black
+    color playButtonColor(255, 0, 0);  // Red
+    color playButtonTextColor(255, 255, 255); // White
 
    // (position x, position y, font size, screen)
     writeHeader(130,100, 12, g);
     writeText(400,400,4,g);
 
     for(int round = 0; round < 10; round++) {
-        drawCircle(l, 50, playButtonColor, g);
-        drawCircle(j, 50, playButtonColor, g);
-        drawCircle(k, 50, playButtonColor, g);
+        //drawCircle(l, 50, playButtonColor, g);
+        //drawCircle(j, 50, playButtonColor, g);
+        //drawCircle(k, 50, playButtonColor, g);
     }
 
     /* NOTE:
@@ -64,22 +78,25 @@ int main(int argc, char ** argv){
         ball.display(g, true);
 
 
-        drawCircle(l, 50, playButtonColor, g);
+        //drawCircle(l, 50, playButtonColor, g);
+        block.drawBlock(p, 50, color1, g);
+        triangle.drawTriangle(g);
+
+
     }
 
 //    Was messing around with the shapes
 //     drawCircle(l, 50, playButtonColor, g);
-//     drawRectangle(j, 50, backgroundColor, g);
+
 //     drawTriangle(k, 50, titleColor, g);
 
 // draws 10 balls. pretty useless but ye
     for (int i=0; i < 10; i++){
         point randPoint (rand() % 800, rand() % 1000);
         size = 50;
-        color ballColor = {255, 23, 23};
+        color ballColor(255, 23, 23);
         drawCircle(randPoint, size, ballColor, g);
     }
-
 
     return 0;
 }
