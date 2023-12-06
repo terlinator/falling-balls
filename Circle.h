96fd1be4758a834fad1dc2d9d86cd294a853ac79
terlinator <118776472+terlinator@users.noreply.github.com>
2023-12-05 18:49:38 -0600

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -20,19 +20,71 @@ class Circle{
 
   public:
   Circle();
+
   Circle(Point, int, int, Color); //Loc, radius, health, color
-
+/*
+ * description: draws a Circle
+ * return: void                                       
+ * precondition: the circle exists               
+ * postcondition: returns nothing
+ *                                                        
+*/
   void drawCircle(SDL_Plotter&);
-
+/*
+ * description: gets the location of the circle
+ * return:   point
+ * precondition:  the circle exists                
+ * postcondition:   returns the point of the circle
+ *                                                        
+*/
   Point getLocation();
+/*
+ * description: sets the location of the circle 
+ * return:  void   
+ * precondition:  the point given is valid
+ * postcondition:  sets the location of the circle to point x
+ *                                                        
+*/
   void setLocation(point x);
-
+/*
+ * description: gets the radius of the circle
+ * return: int                                    
+ * precondition: radius is valid                    
+ * postcondition: returns an int               
+ *                                                        
+*/
   int getRadius();
+/*
+ * description: sets the radius of the circle
+ * return: void                                          
+ * precondition: the data provided is valid               
+ * postcondition: sets the radius                  
+ *                                                        
+*/
   void setRadius(int x);
-
+/*
+ * description: gets the health of the returns 
+ * return: int                                 
+ * precondition: the ball is valid                           
+ * postcondition: returns an int                 
+ *                                                        
+*/
   int getHealth();
+/*
+ * description:   sets the ball health
+ * return:   void                                        
+ * precondition:  the ball exists                     
+ * postcondition: the ball's health value is set                 
+ *                                                        
+*/
   void setHealth(int val);
-
+/*
+ * description: checks if the ball is colliding
+ * return: bool                                          
+ * precondition: the ball exists                        
+ * postcondition: returns true or false              
+ *                                                        
+*/
   bool collisionCheck(const Ball&);
 };
 
