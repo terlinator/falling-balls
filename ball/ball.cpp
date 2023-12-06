b5a1e652ae5958e25c8b9bdfaae85498de45559d
AOM3232 <150971628+AOM3232@users.noreply.github.com>
2023-12-05 19:01:00 -0600

    Update ball.cpp

--- ball/ball.cpp
+++ ball/ball.cpp
@@ -15,9 +15,12 @@ Ball::Ball() {
     loc.x = 80;
     loc.y = 80;
 
+    prevLoc.x = 80;
+    prevLoc.y = 80;
+
     _color.R = 255;
-    _color.G = 255;
-    _color.B = 255;
+    _color.G = 25;
+    _color.B = 25;
 
     radius = 25;
     f.setDirection(PI / 2);
@@ -28,29 +31,42 @@ void Ball::setColor(color c) {
     _color = c;
 }
 
-void Ball::setLocation(point p){
+void Ball::setLocation(Point p){
     loc = p;
+}
+void Ball::setPrevLocation(Point p){
+    prevLoc = p;
 }
 void Ball::setRadius(int r) {
     radius = r;
+}
+void Ball::setForce(force f2) {
+    f.setDirection(f2.getDirection());
+    f.setMagnitude(f2.getMagnitude());
 }
 
 color Ball::getColor() const{
     return _color;
 }
-const point Ball::getLocation() const{
+const Point Ball::getLocation() const{
     return loc;
+}
+const Point Ball::getPrevLocation() const{
+    return prevLoc;
 }
 const int Ball::getRadius() const {
     return radius;
+}
+const force Ball::getForce() const {
+    return f;
 }
 
 void Ball::display(SDL_Plotter& g, bool ERASE) {
     color c = _color;
     if (ERASE) {
         c.R = 255;
-        c.G = 25;
-        c.B = 25;
+        c.G = 255;
+        c.B = 255;
     }
     for (int x = -radius; x <= radius; x++) {
         for (int y = -radius; y <= radius; y++) {
@@ -60,10 +76,12 @@ void Ball::display(SDL_Plotter& g, bool ERASE) {
         }
     }
 }
-void Ball::move() {
-    loc.x += f.getMagnitude() * cos(f.getDirection());
-    loc.y += f.getMagnitude() * sin(f.getDirection());
+/*void Ball::move() {
+    prevLoc.x += f.getMagnitude() * cos(f.getDirection());
+    prevLoc.y += f.getMagnitude() * sin(f.getDirection());
     f.apply(GRAVITY);
+
+
     if(loc.y >= (SCREEN_SIZE_HEIGHT + radius)) {
         f.setDirection((3 * PI) / 2);
         f.setMagnitude(0.5* f.getMagnitude());
@@ -75,4 +93,4 @@ void Ball::move() {
 
 
 
-}
+}*/
