ed1d0b41afb78f86b3cd4c1cf8634ba2fdcecf06
CamKHardin <150968844+CamKHardin@users.noreply.github.com>
2023-12-06 16:41:37 -0600

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,5 +1,5 @@
 //Group Twelve Names: Daniel Esquivel, Briel Finley,
-//	Cameron Hardin, Sterling Matthews, Andrew Meador
+//    Cameron Hardin, Sterling Matthews, Andrew Meador
 //Project Name: Falling Balls
 //Assignment Description: Create a constant
 //File Name: constans.h
@@ -8,11 +8,11 @@
 #ifndef CONSTANTS_H_INCLUDED
 #define CONSTANTS_H_INCLUDED
 
-#include "Force.h"
+#include "force.h"
 
 const int SCREEN_SIZE_WIDTH = 800;
-const int SCREEN_SIZE_HEIGHT = 800;
-const int OBJECT_SIZE = 25;
-const force GRAVITY(0.001, PI/2);
+const int SCREEN_SIZE_HEIGHT = 500;
+const int OBJECT_SIZE = 150;
+//const force GRAVITY(0.001, PI/2);
 
 #endif // CONSTANTS_H_INCLUDED
