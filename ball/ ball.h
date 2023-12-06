62a6d8a6a2b123183d0a3ba64d6a3dd0221e4313
CamKHardin <150968844+CamKHardin@users.noreply.github.com>
2023-12-06 16:42:08 -0600

    Update  ball.h

--- ball/ ball.h
+++ ball/ ball.h
@@ -1,5 +1,5 @@
 //Group Twelve Names: Daniel Esquivel, Briel Finley,
-//	Cameron Hardin, Sterling Matthews, Andrew Meador
+//    Cameron Hardin, Sterling Matthews, Andrew Meador
 //Project Name: Falling Balls
 //Assignment Description: Create a ball class
 //File Name: ball.h
@@ -9,23 +9,12 @@
 #define BALL_H_INCLUDED
 
 #include <cmath>
-#include "SDL_Plotter.h"
-#include "Force.h"
-#include "point.h"
+#include "SDL_Plotter.hpp"
+#include "force.h"
+#include "Point.h"
 
 using namespace std;
 
-/*struct point_t{
-    double x, y;
-    point_t(){
-        x = y = 0;
-    }
-
-    point_t(double x, double y){
-        this->x = x;
-        this->y = y;
-    }
-};*/
 
 class Ball {
     private:
@@ -46,20 +35,21 @@ class Ball {
         Ball();
 
         void setColor(color);
-        void setLocation(Point);
-        void setPrevLocation(Point);
+        void setLoc(Point);
+        void setPrevLoc(Point);
         void setRadius(int);
         void setForce(force);
 
         color getColor() const;
-        const Point getLocation() const;
-        const Point getPrevLocation() const;
+        const Point getLoc() const;
+        const Point getPrevLoc() const;
         const int getRadius() const;
         const force getForce() const;
 
         void display(SDL_Plotter&, bool = false);
-        //void move();
+        void move();
 
 };
 
 #endif // BALL_H_INCLUDED
+
