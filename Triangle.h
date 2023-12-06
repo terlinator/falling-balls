8e7046ac74c76f037e212f3d2e7c9df7605c7694
AOM3232 <150971628+AOM3232@users.noreply.github.com>
2023-12-05 22:37:57 -0600

    Update Triangle.h

--- Triangle.h
+++ Triangle.h
@@ -5,6 +5,7 @@
 //File Name: Triangle.h
 //File Created: 11/14/2023
 //File Last Edited: 12/05/2023
+
 #ifndef TRIANGLE_H_INCLUDED
 #define TRIANGLE_H_INCLUDED
 
@@ -22,61 +23,28 @@ class Triangle{
   public:
   Triangle();
   Triangle(Point, int, int, color); //Loc, size, health, color
-/*
- * description: 
- * return:   point
- * precondition:  the circle exists                
- * postcondition:   returns the point of the circle
- *                                                        
-*/
-  void drawTriangle(SDL1_Plotter&);
-/*
- * description: gets the location of the circle
- * return:   point
- * precondition:  the circle exists                
- * postcondition:   returns the point of the circle
- *                                                        
-*/
+  /*
+  *description: draws triangle to screen
+  *return: point
+  *precondition: the circle exists
+  *postcondition: returns the point of the circle
+  */
+  void drawTriangle(SDL_Plotter&);
+
+  /*
+  *description: draws triangle to screen
+  *return: point
+  *precondition: the circle exists
+  *postcondition: returns the point of the circle
+  */
   Point getLoc() const;
-/*
- * description: gets the location of the circle
- * return:   point
- * precondition:  the circle exists                
- * postcondition:   returns the point of the circle
- *                                                        
-*/
+
   void setLoc(Point x);
-/*
- * description: gets the location of the circle
- * return:   point
- * precondition:  the circle exists                
- * postcondition:   returns the point of the circle
- *                                                        
-*/
+
   int getSide() const;
-/*
- * description: gets the location of the circle
- * return:   point
- * precondition:  the circle exists                
- * postcondition:   returns the point of the circle
- *                                                        
-*/
   void setSide(int x);
-/*
- * description: gets the location of the circle
- * return:   point
- * precondition:  the circle exists                
- * postcondition:   returns the point of the circle
- *                                                        
-*/
+
   int getHealth() const;
-/*
- * description: gets the location of the circle
- * return:   point
- * precondition:  the circle exists                
- * postcondition:   returns the point of the circle
- *                                                        
-*/
   void setHealth(int val);
 
   //bool collisionCheck(const Ball&);
