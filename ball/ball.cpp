1cfb90eda2861660a813fd0899d84614905a8777
terlinator <118776472+terlinator@users.noreply.github.com>
2023-12-05 18:00:27 -0600

    Update ball.cpp

--- ball/ball.cpp
+++ ball/ball.cpp
@@ -1,3 +1,10 @@
+//Group Twelve Names: Daniel Esquivel, Briel Finley,
+//	Cameron Hardin, Sterling Matthews, Andrew Meador
+//Project Name: Falling Balls
+//Assignment Description: Create definitions for the ball functions
+//File Name: ball.cpp
+//File Created: 11/14/2023
+//File Last Edited: 12/05/2023
 #include "ball.h"
 #include "constants.h"
 
