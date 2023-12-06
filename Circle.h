a62878f5ab423f4f84efe3cb3cd3c74256f5c4e9
CamKHardin <150968844+CamKHardin@users.noreply.github.com>
2023-12-06 16:43:32 -0600

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -1,5 +1,5 @@
 //Group Twelve Names: Daniel Esquivel, Briel Finley,
-//	Cameron Hardin, Sterling Matthews, Andrew Meador
+//    Cameron Hardin, Sterling Matthews, Andrew Meador
 //Project Name: Falling Balls
 //Assignment Description: Create a triangle header
 //File Name: Circle.h
@@ -9,81 +9,83 @@
 #ifndef CIRCLE_H_
 #define CIRCLE_H_
 
-#include "SDL_Plotter/SDL_Plotter.h"
+#include "SDL_Plotter.hpp"
+#include "Point.h"
+#include "ball.h"
 
 class Circle{
   private:
   Point loc;
   int health;
   int radius;
-  Color color;
+  color color1;
 
   public:
   Circle();
 
-  Circle(Point, int, int, Color); //Loc, radius, health, color
+  Circle(Point, int, int, struct color); //Loc, radius, health, color
 /*
  * description: draws a Circle
- * return: void                                       
- * precondition: the circle exists               
+ * return: void
+ * precondition: the circle exists
  * postcondition: returns nothing
- *                                                        
+ *
 */
   void drawCircle(SDL_Plotter&);
 /*
  * description: gets the location of the circle
  * return:   point
- * precondition:  the circle exists                
+ * precondition:  the circle exists
  * postcondition:   returns the point of the circle
- *                                                        
+ *
 */
-  Point getLocation();
+  Point getLoc();
 /*
- * description: sets the location of the circle 
- * return:  void   
+ * description: sets the location of the circle
+ * return:  void
  * precondition:  the point given is valid
  * postcondition:  sets the location of the circle to point x
- *                                                        
+ *
 */
-  void setLocation(point x);
+  void setLoc(point x);
 /*
  * description: gets the radius of the circle
- * return: int                                    
- * precondition: radius is valid                    
- * postcondition: returns an int               
- *                                                        
+ * return: int
+ * precondition: radius is valid
+ * postcondition: returns an int
+ *
 */
   int getRadius();
 /*
  * description: sets the radius of the circle
- * return: void                                          
- * precondition: the data provided is valid               
- * postcondition: sets the radius                  
- *                                                        
+ * return: void
+ * precondition: the data provided is valid
+ * postcondition: sets the radius
+ *
 */
   void setRadius(int x);
 /*
- * description: gets the health of the returns 
- * return: int                                 
- * precondition: the ball is valid                           
- * postcondition: returns an int                 
- *                                                        
+ * description: gets the health of the returns
+ * return: int
+ * precondition: the ball is valid
+ * postcondition: returns an int
+ *
 */
   int getHealth();
 /*
  * description:   sets the ball health
- * return:   void                                        
- * precondition:  the ball exists                     
- * postcondition: the ball's health value is set                 
- *                                                        
+ * return:   void
+ * precondition:  the ball exists
+ * postcondition: the ball's health value is set
+ *
 */
   void setHealth(int val);
 /*
  * description: checks if the ball is colliding
- * return: bool                                          
- * precondition: the ball exists                        
- * postcondition: returns true or false              
- *                                                        
+ * return: bool
+ * precondition: the ball exists
+ * postcondition: returns true or false
+ *
 */
   bool collisionCheck(const Ball&);
 };
