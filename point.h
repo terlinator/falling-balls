3730c81c4699a03d92e8a01dab4b51453fee88d2
terlinator <118776472+terlinator@users.noreply.github.com>
2023-12-06 21:36:50 -0600

    Update point.h

--- point.h
+++ point.h
@@ -16,13 +16,25 @@ struct Point{
       this->x = x;
       this->y = y;
    }
-
+/*
+ * description: overloads the equals operator
+ * return: point reference
+ * precondition: point given is valid
+ * postcondition: returns *this
+ *
+*/
    Point& operator= (const Point& p){
        x = p.x;
        y = p.y;
        return *this;
    }
-
+/*
+ * description: returns the distance from the point
+ * return: double
+ * precondition: the the point given is valid
+ * postcondition: returns the distance from the point as a double
+ *
+*/
    double distance(Point p) const{
        return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
    }
