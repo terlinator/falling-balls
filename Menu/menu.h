d1607cce948e536cb9ee654f0b686c161c626128
AriannaBriel <149217807+AriannaBriel@users.noreply.github.com>
2023-12-06 22:44:13 -0600

    Update menu.h

--- Menu/menu.h
+++ Menu/menu.h
@@ -4,26 +4,93 @@
 //File Name: menu.h
 //File Created: 11/14/2023
 //File Last Edited: 12/05/2023, Andrew Meador
+#ifndef GROUPPROJECT_MENU_H
+#define GROUPPROJECT_MENU_H
 
-#include <iostream>
-
-#include "screen.h"
+#include "../SDL_Plotter/SDL_Plotter.h"
+#include "../Screen/screen.h"
+// #include "../constants.h" // Data
+#include <cmath>
+#include <fstream>
+#include <sstream>
+using namespace std;
 /*
- * description: writes the text to a point
- * return: void
- * precondition: ints provided are valid
- * postcondition: drwas the text in a set point
- *
+* description: writes the text to the screen
+* return: void
+* precondition: the ints provided are valid
+* postcondition: writes the header to the set point
 */
-void writeText(int posX, int posY, 
-int SizeOfFont, SDL_Plotter& homeScreen);
+void writeToScreen(const string& filename, int posX, int posY, int SizeOfFont,
+                   SDL_Plotter& homeScreen);
 /*
- * description: writes the header text
- * return: void
- * precondition: the ints provided are valid
- * postcondition: writes the header to the set point
- *
+* description: writes the title page text
+* return: void
+* precondition: the ints provided are valid
+* postcondition: writes the header to the set point
 */
-void writeHeader(int posX, int posY, 
-int SizeOfFont, SDL_Plotter& homeScreen);
+void writeStart(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen);
+/*
+* description: writes the header text
+* return: void
+* precondition: the ints provided are valid
+* postcondition: writes the header to the set point
+*/
+void writeFallingBalls(int posX, int posY, int SizeOfFont, SDL_Plotter&
+                        homeScreen);
+/*
+* description: writes the leaderboard text
+* return: void
+* precondition: the ints provided are valid
+* postcondition: writes the header to the set point
+*/
+void writeLeaderboard(int posX, int posY, int SizeOfFont, SDL_Plotter&
+                        homeScreen);
+/*
+* description: writes the credits text
+* return: void
+* precondition: the ints provided are valid
+* postcondition: writes the header to the set point
+*/
+void writeCredits(int posX, int posY, int SizeOfFont, SDL_Plotter&
+                    homeScreen);
+/*
+* description: writes the back button text
+* return: void
+* precondition: the ints provided are valid
+* postcondition: writes the header to the set point
+*/
+void writeBack(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen);
+/*
+* description: writes the pause button text
+* return: void
+* precondition: the ints provided are valid
+* postcondition: writes the header to the set point
+*/
+void writePause(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen);
+/*
+* description: writes the options text
+* return: void
+* precondition: the ints provided are valid
+* postcondition: writes the header to the set point
+*/
+void writeOptions(int posX, int posY, int SizeOfFont, SDL_Plotter&
+                homescreen);
 
+/*
+* description: draws a circle
+* return: void
+* precondition: the point and int provided is valid
+* postcondition: draws the circle to the set point
+*/
+void drawCircle(point loc, int size, color c, SDL_Plotter& g);
+
+/*
+* description: draws a rectangle
+* return: void
+* precondition: the point and int provided is valid
+* postcondition: draws the rectangle to the set point
+*/
+void drawRectangle(point loc, int size, color c, SDL_Plotter &g,
+                   int i, int i1, int i2, SDL_Plotter plotter);
+
+#endif //GROUPPROJECT_MENU_H
