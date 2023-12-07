a76aab2e908541f06e9c3eaf04b7cababeae5a4c
AriannaBriel <149217807+AriannaBriel@users.noreply.github.com>
2023-12-06 22:45:58 -0600

    Update menu.cpp

--- Menu/menu.cpp
+++ Menu/menu.cpp
@@ -7,84 +7,84 @@
 
 #include <iostream>
 
-void writeText(int posX, int posY, int SizeOfFont, 
-SDL_Plotter& homeScreen) {
+void writeToScreen(const string& filename, int posX, int posY, int SizeOfFont,
+                   SDL_Plotter& homeScreen) {
+
     ifstream infile;
     string line, value;
     int col, r, g, b, row = 0;
 
-    infile.open("sheet.csv");
+    infile.open(filename);
+
     while (getline(infile, line)) {
         stringstream ss(line);
         col = 0;
         while (getline(ss, value, ',')) {
             int pixelValue = stoi(value);
             int pixelSize = SizeOfFont;
+            r = 0;
+            g = 0;
+            b = 0;
 
-            // Prints black if number is 1, white for anything else
-            if(pixelValue == 1){
-                r = 0;
-                g = 0;
-                b = 0;
-            }
-            else{
-                r = 255;
-                g = 255;
-                b = 255;
-            }
-
-            // Draw the pixel
-            for (int x = 0; x < pixelSize; ++x) {
-                for (int y = 0; y < pixelSize; ++y) {
-                    homeScreen.plotPixel(posX + col * pixelSize + x, 
-                        posY + row * pixelSize + y, r, g, b);
+            for (int i = 0; i <= row; i++) {
+                b += 35;
+                r += 5;
+                for (int l = 0; l <= col; l++) {
+                    if (pixelValue == 0) {
+                        r = 255;
+                        g = 255;
+                        b = 255;
+                    }
                 }
+
+                // Draw the pixel
+                for (int x = 0; x < pixelSize; ++x) {
+                    for (int y = 0; y < pixelSize; ++y) {
+                        homeScreen.plotPixel(posX + col * pixelSize + x,
+                                             posY + row * pixelSize + y, r,
+                                             g, b);
+                    }
+                }
+                col++;
             }
-
-            col++;
+            row++;
         }
-
-        row++;
     }
 }
-
-void writeHeader(int posX, int posY, int SizeOfFont, 
-SDL_Plotter& homeScreen) {
-    ifstream infile;
-    string line, value;
-    int col, r, g, b, row = 0;
-
-    infile.open("start.csv");
-    while (getline(infile, line)) {
-        stringstream ss(line);
-        col = 0;
-        while (getline(ss, value, ',')) {
-            int pixelValue = stoi(value);
-            int pixelSize = SizeOfFont;
-
-            // Prints black if number is 1, white for anything else
-            if(pixelValue == 1){
-                r = 0;
-                g = 0;
-                b = 0;
-            }
-            else{
-                r = 255;
-                g = 255;
-                b = 255;
-            }
-
-            // Draw the pixel
-            for (int x = 0; x < pixelSize; ++x) {
-                for (int y = 0; y < pixelSize; ++y) {
-                    homeScreen.plotPixel(posX + col * pixelSize + x, 
-                        posY + row * pixelSize + y, r, g, b);
-                }
-            }
-
-            col++;
-        }
-
-        row++;
-    }
+void writeFallingBalls(int posX, int posY, int SizeOfFont,
+                       SDL_Plotter& homeScreen){
+    writeToScreen("Letters/CSV sheets/sheet.csv", posX,
+                  posY, SizeOfFont, homeScreen);
 }
+
+void writeStart(int posX, int posY, int SizeOfFont,
+                SDL_Plotter& homeScreen) {
+    writeToScreen("Letters/CSV sheets/start.csv",
+                  posX, posY, SizeOfFont, homeScreen);
+}
+
+void writeOptions(int posX, int posY, int SizeOfFont,
+                  SDL_Plotter& homeScreen) {
+    writeToScreen("Letters/CSV sheets/options.csv",
+                  posX, posY, SizeOfFont, homeScreen);
+}
+void writeLeaderboard(int posX, int posY, int SizeOfFont,
+                      SDL_Plotter& homeScreen){
+    writeToScreen("Letters/CSV sheets/Leaderboard.csv",
+                  posX, posY, SizeOfFont, homeScreen);
+}
+
+void writeCredits(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen){
+    writeToScreen("Letters/CSV sheets/credits.csv", posX, posY, SizeOfFont,
+                  homeScreen);
+}
+
+void writeBack(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen){
+    writeToScreen("Letters/CSV sheets/back.csv", posX, posY, SizeOfFont,
+                  homeScreen);
+}
+
+void writePause(int posX, int posY, int SizeOfFont, SDL_Plotter& homeScreen){
+    writeToScreen("Letters/CSV sheets/pause.csv", posX, posY, SizeOfFont,
+                  homeScreen);
+
