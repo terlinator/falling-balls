401197d3a23f853bdca8d3d0b5e4a1e871005dfa
terlinator <118776472+terlinator@users.noreply.github.com>
2023-12-06 21:39:37 -0600

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -4,7 +4,7 @@
 //Assignment Description: Create a triangle header
 //File Name: Triangle.cpp
 //File Created: 11/14/2023
-//File Last Edited: 12/05/2023
+//File Last Edited: 12/06/2023
 
 #include "Triangle.h"
 
